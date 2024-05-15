6f183ce8219975f2f3317b4f9774964dc8415b74
jiahangchun <jiahangchun@163.com>
2024-05-16 00:02:24 +0800

    feishu demo

--- src/test/java/third/feishu/message/MessageServiceTest.java
+++ src/test/java/third/feishu/message/MessageServiceTest.java
@@ -1,26 +1,26 @@
-package third.feishu.message;
-
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import third.feishu.message.MessageService;
-
-/**
- * Created by IntelliJ IDEA.
- *
- * @Author : JIA
- */
-@SpringBootTest
-public class MessageServiceTest {
-
-    @Resource
-    private MessageService messageService;
-
-    @Test
-    public void a() throws Exception {
-        messageService.sendMessage("","");
-
-    }
-
-}
+//package third.feishu.message;
+//
+//import jakarta.annotation.Resource;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import third.feishu.message.MessageService;
+//
+///**
+// * Created by IntelliJ IDEA.
+// *
+// * @Author : JIA
+// */
+//@SpringBootTest
+//public class MessageServiceTest {
+//
+//    @Resource
+//    private MessageService messageService;
+//
+//    @Test
+//    public void a() throws Exception {
+//        messageService.sendMessage("","");
+//
+//    }
+//
+//}
