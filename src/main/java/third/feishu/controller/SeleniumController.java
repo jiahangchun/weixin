31c62d08c32da595499152502d7812178902e782
jiahangchun <jiahangchun@163.com>
2024-05-31 00:39:30 +0800

    selenium

--- src/main/java/third/feishu/controller/SeleniumController.java
+++ src/main/java/third/feishu/controller/SeleniumController.java
@@ -26,73 +26,71 @@ public class SeleniumController {
 
     @Async
     public void getNovel1(int size) {
-        String novelUrl = "https://www.bigee.cc/book/33700/";
-        String drivePath = "C:\\jiahangchun\\idea\\code\\chromdriver";
-        System.setProperty("webdriver.chrom.driver", drivePath);
+        try {
+            String novelUrl = "https://www.bigee.cc/book/33700/";
+            String drivePath = "C:\\jiahangchun\\idea\\code\\chromdriver.exe";
+            System.setProperty("webdriver.chrom.driver", drivePath);
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.get(novelUrl);
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+            driver.get(novelUrl);
 
-        WebElement textBox = driver.findElement(By.className("listmain"));
-        List<WebElement> elements = textBox.findElements(By.cssSelector("a"));
+            WebElement textBox = driver.findElement(By.className("listmain"));
+            List<WebElement> elements = textBox.findElements(By.cssSelector("a"));
 
-        Map<String, String> map = new LinkedHashMap();
-        for (WebElement webElement : elements) {
-            String name = webElement.getText();
-            String link = webElement.getAttribute("href");
-            if (name.contains("章") && name.contains("第")) {
-                map.put(name, link);
-            }
-        }
-
-        int i = 0;
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            i++;
-            try {
-                String title = entry.getKey();
-                String link = entry.getValue();
-                driver.get(link);
-                WebElement contentElement = driver.findElement(By.id("chaptercontent"));
-                String content = contentElement.getText();
-                content = content.replaceAll("“", "");
-                content = content.replaceAll("”", "");
-                String[] list = content.split("\n\n");
-                //讲size章的时候中断
-                if (i > size) {
-                    break;
+            Map<String, String> map = new LinkedHashMap();
+            for (WebElement webElement : elements) {
+                String name = webElement.getText();
+                String link = webElement.getAttribute("href");
+                if (name.contains("章") && name.contains("第")) {
+                    map.put(name, link);
                 }
-                for (String s : list) {
-                    if (s.contains("点此报错") || s.contains("请收藏本站")) {
-                        continue;
+            }
+
+            int i = 0;
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                i++;
+                try {
+                    String title = entry.getKey();
+                    String link = entry.getValue();
+                    driver.get(link);
+                    WebElement contentElement = driver.findElement(By.id("chaptercontent"));
+                    String content = contentElement.getText();
+                    content = content.replaceAll("“", "");
+                    content = content.replaceAll("”", "");
+                    String[] list = content.split("\n\n");
+                    //讲size章的时候中断
+                    if (i > size) {
+                        break;
                     }
-                    String resp = chatClient.call("小说后续是这么发展的：" + s);
-                    System.out.println(title + "---->" + s);
-                    System.out.println(title + "=======================>" + resp);
-                }
-                System.out.println(title + " …………………………………………………………………… 讲完了");
+                    for (String s : list) {
+                        if (s.contains("点此报错") || s.contains("请收藏本站")) {
+                            continue;
+                        }
+                        String resp = chatClient.call("小说后续是这么发展的：" + s);
+                        System.out.println(title + "---->" + s);
+                        System.out.println(title + "=======================>" + resp);
+                    }
+                    System.out.println(title + " …………………………………………………………………… 讲完了");
+                } catch (Exception e) {
+                    log.info("error {}", e.getMessage(), e);
+                    try {
+                        Thread.sleep(60 * 1000);
+                    } catch (InterruptedException ex) {
 
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-
-                }
-            } catch (Exception e) {
-                log.info("error {}", e.getMessage(), e);
-                try {
-                    Thread.sleep(60 * 1000);
-                } catch (InterruptedException ex) {
-
+                    }
                 }
             }
-        }
 
 
-        for (int k = 0; k < 1000; k++) {
-            String value = chatClient.call("你猜后续会怎么发展？请用200字描述下");
-            System.out.println(k + "ooooooooooooooooooooo" + value);
+            for (int k = 0; k < 1000; k++) {
+                String value = chatClient.call("你猜后续会怎么发展？请用200字描述下");
+                System.out.println(k + "ooooooooooooooooooooo" + value);
+            }
+            driver.quit();
+        } catch (Exception e) {
+            log.info("error ========== {}", e.getMessage(), e);
         }
-        driver.quit();
     }
 
     /**
